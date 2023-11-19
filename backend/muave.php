46b247d3e1c37eddc025707b4c9585daec7726cd
TaiZuon <124855937+TaiZuon@users.noreply.github.com>
2023-11-19 22:49:53 +0700

    add muave

--- backend/muave.php
+++ backend/muave.php
@@ -1,218 +1,88 @@
-
-
-<!-- <?php
-          //list available flight 
-            try {
-              $listOfFlight = array(array());
-
-              require_once "../helper/connect_dtb.php";
-              $diem_xuatphat = $_POST['departureCity'];
-              $diem_den = $_POST['arrivalCity'];
-              $thoidiem_den = date("Y/m/d",strtotime($_POST['returnDate']));
-              $thoidiem_di = $_POST['departureDate'];
-
-              $sql = "SELECT FlightID FROM flight 
-              WHERE diem_den = \"$diem_den\"
-              AND diem_xuatphat = \"$diem_xuatphat\"
-              AND thoidiem_den <= \"$thoidiem_den 00:00:00\"
-              AND thoidiem_di >= \"$thoidiem_di 23:59:59\";";
-
-              // echo $sql;
-              $result = mysqli_query($conn, $sql);
-
-              if (mysqli_num_rows($result) > 0) {
-                while ($row = mysqli_fetch_array($result)) {
-                  //dem so ve 
-                  $FlightID = $row['FlightID'];
-                  $travelClass = $_POST['seatingPreference'];
-                  $sql1 = "SELECT COUNT(*) as num FROM booking JOIN travelclass WHERE booking.FlightID = \"$FlightID\" AND travelclass.TravelClassCode = \"$travelClass\";";
-                  $soldTicket = mysqli_query($conn, $sql1);
-                  $num = (int)mysqli_fetch_array($soldTicket)['num'];
-
-                  //so ve toi da ung voi travelclass
-                  $sql2 = "SELECT so_ve FROM travelclass WHERE TravelClassCode = \"$travelClass\"; ";
-                  $capacity = mysqli_query($conn, $sql2);
-                  $numOfSeats = (int)mysqli_fetch_array($capacity)['so_ve'];
-                  
-                  //neu ve chua full
-                  if ($num < $numOfSeats) {
-                    //tao form cho user pick
-                      //lay thong tin ve flight
-                      $sql3 = "SELECT * FROM flight WHERE FlightID = \"$FlightID\";";
-                      $info = mysqli_query($conn, $sql3);
-                      $flightInfo = mysqli_fetch_array($info);
-                      array_push($listOfFlight, array('FlightID'=>$FlightID,'travelClass'=>$travelClass));
-                  }
-                }
-              }
-              echo '<pre>';
-              print_r($listOfFlight);
-              echo '</pre>';
-              // setcookie('listOfFlight',$listOfFlight, 86400, '/');
-
-            }catch(Exception $e) {
-              echo $e->getMessage();
-            }
-          ?> -->
+<link rel="stylesheet" href="../asset/css/muave.css">
 
 <?php
-// setcookie('FlightID','123',time()- 86400);
-// setcookie('username', 'John Doe', time()- 3600);
-
-  try {
-    if(isset($_POST['myInput'])) {
-      setcookie('FlightID',$FlightID,86400,'/');
-    }
-  } catch (Exception $e) {
-    echo $e->getMessage();
-  }
-
-//setcookie('travelClass',$travelClass,86400,'/');
-//}
-echo '<pre>';
-    print_r($_COOKIE);
-    echo '</pre>';
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Flight Information</title>
-  <link rel="stylesheet" href="style.css">
-</head>
-<body>
-  <main>
-    <div class="container">
-      <div class="flight-information">
-        <h1>Flight Information</h1>
-
-        <div class="card">
-          <div class="card-header">Flight Details</div>
-          <div class="card-body">
-
-          <?php
           //list available flight 
-            try {
-              require_once "../helper/connect_dtb.php";
-              $diem_xuatphat = $_POST['departureCity'];
-              $diem_den = $_POST['arrivalCity'];
-              $thoidiem_den = date("Y/m/d",strtotime($_POST['returnDate']));
-              $thoidiem_di = $_POST['departureDate'];
+            $listOfFlight = array(array());
 
-              echo '<br>';
+            require_once "../helper/connect_dtb.php";
+            $diem_xuatphat = $_POST['departureCity'];
+            $diem_den = $_POST['arrivalCity'];
+            $thoidiem_den = date("Y/m/d",strtotime($_POST['returnDate']));
+            $thoidiem_di = $_POST['departureDate'];
+            $numOfPass = $_POST['passengers'];
 
-              $sql = "SELECT FlightID FROM flight 
-              WHERE diem_den = \"$diem_den\"
-              AND diem_xuatphat = \"$diem_xuatphat\"
-              AND thoidiem_den <= \"$thoidiem_den 00:00:00\"
-              AND thoidiem_di >= \"$thoidiem_di 23:59:59\";";
+            $sql = "SELECT FlightID FROM flight 
+            WHERE diem_den = \"$diem_den\"
+            AND diem_xuatphat = \"$diem_xuatphat\"
+            AND thoidiem_den <= \"$thoidiem_den 00:00:00\"
+            AND thoidiem_di >= \"$thoidiem_di 23:59:59\";";
+            $result = mysqli_query($conn, $sql);
 
-              // echo $sql;
+            ?>
+            <table class="table">
+              <thead>
+                <tr class="text-center">
+                  <th scope="col">Departure</th>
+                  <th scope="col">Arrival</th>
+                  <th scope="col">Status</th>
+                  <th scope="col">Fare</th>
+                  <th scope="col">Buy</th>
+                </tr>
+              </thead>
+              <tbody>
+            <?php
+            if (mysqli_num_rows($result) > 0) {
+              while ($row = mysqli_fetch_array($result)) {
+                //dem so ve 
+                $FlightID = $row['FlightID'];
+                $travelClass = $_POST['seatingPreference'];
+                $sql1 = "SELECT COUNT(*) as num FROM booking JOIN travelclass WHERE booking.FlightID = \"$FlightID\" AND travelclass.TravelClassCode = \"$travelClass\";";
+                $soldTicket = mysqli_query($conn, $sql1);
+                $num = (int)mysqli_fetch_array($soldTicket)['num'];
 
-              $result = mysqli_query($conn, $sql);
-
-              echo '<br>';
-              if (mysqli_num_rows($result) > 0) {
-                while ($row = mysqli_fetch_array($result)) {
-                  //dem so ve 
-                  $FlightID = $row['FlightID'];
-                  
-                  $travelClass = $_POST['seatingPreference'];
-                  $sql1 = "SELECT COUNT(*) as num FROM booking JOIN travelclass WHERE booking.FlightID = \"$FlightID\" AND travelclass.TravelClassCode = \"$travelClass\";";
-                  $soldTicket = mysqli_query($conn, $sql1);
-                  $num = (int)mysqli_fetch_array($soldTicket)['num'];
-
-                  //so ve toi da ung voi travelclass
-                  $sql2 = "SELECT so_ve FROM travelclass WHERE TravelClassCode = \"$travelClass\"; ";
-                  $capacity = mysqli_query($conn, $sql2);
-                  $numOfSeats = (int)mysqli_fetch_array($capacity)['so_ve'];
-                  
-                  //neu ve chua full
-                  if ($num < $numOfSeats) {
-                    //tao form cho user pick
-                      //lay thong tin ve flight
-                      $sql3 = "SELECT * FROM flight WHERE FlightID = \"$FlightID\";";
-                      $info = mysqli_query($conn, $sql3);
-                      $flightInfo = mysqli_fetch_array($info);           
+                //so ve toi da ung voi travelclass
+                $sql2 = "SELECT so_ve FROM travelclass WHERE TravelClassCode = \"$travelClass\"; ";
+                $capacity = mysqli_query($conn, $sql2);
+                $numOfSeats = (int)mysqli_fetch_array($capacity)['so_ve'];
+                
+                //neu ve chua full
+                if ($numOfSeats - $num >= $numOfPass) {
+                  //tao form cho user pick
+                    //lay thong tin ve flight
+                    $sql3 = "SELECT * FROM flight WHERE FlightID = \"$FlightID\";";
+                    $info = mysqli_query($conn, $sql3);
+                    $flightInfo = mysqli_fetch_array($info);
+                    $price = $flightInfo['gia_ve'];
+                    $status = $flightInfo['trang_thai'];
+                    $arrival_time = $flightInfo['thoidiem_den'];
+                    $departure_time = $flightInfo['thoidiem_di'];
+                    array_push($listOfFlight, array('FlightID'=>$FlightID,'travelClass'=>$travelClass));
                     
-                    ?>
-                    <?php
-                      if(isset($_POST['sumbit'])) {
-                        session_start();
-                        $_SESSION["FlightID"] = $FlightID;
-                        header("Location: temp.php");
-                        die();
-                      }
-                    ?>
-                    <form method ="post" action= "muave.php">
-                    
-                    <div class="tab-pane fade" id="flight-pane">
-                      <div class="card">
-                        <div class="card-header">
-                          <h5 class="card-title">Flight VN</h5>
-                        </div>
-                        <div class="card-body">
-                          <table class="table table-striped">
-                            <tbody>
-                              <tr>
-                                <td>Departure airport</td>
-                                <td><?php echo $diem_xuatphat?></td>
-                              </tr>
-                              <tr>
-                                <td>Arrival airport</td>
-                                <td><?php echo $diem_den?></td>
-                              </tr>
-                              <tr>
-                                <td>Departure time</td>
-                                <td><?php echo $flightInfo['thoidiem_di']?></td>
-                              </tr>
-                              <tr>
-                                <td>Arrival time</td>
-                                <td><?php echo $flightInfo['thoidiem_den']?></td>
-                              </tr>
-                              <tr>
-                                <td>Duration</td>
-                                <td><?php echo 'thoi gian di'?></td>
-                              </tr>
-                              <tr>
-                                <td>Price</td>
-                                <td><?php echo $flightInfo['gia_ve']?></td>
-                              </tr>
-                              <tr>
-                                <td>Availability</td>
-                                <td><?php echo $numOfSeats - $num?> seat left</td>
-                              </tr>
-                              <tr>
-                                <td></td>
-                                <td>
-                                    <button type="submit" class="btn btn-primary" name="submit">Book now</button>
-                                </td>
-                              
-                              </tr>
-                            </tbody>
-                          </table>
-                        </div>
-                      </div>
-                    </div>
+                    echo "
+                    <tr class='text-center'>                  
+                      <td>".$departure_time."</td>
+                      <td>".$arrival_time."</td>
+                      <td>$status</td>   
+                      <td>$ ".$price."</td>
+                    ";
+
+                    echo " <td>
+                    <form action='nhapthongtin.php' method='post'>
+                    <input name='flight_id' type='hidden' value=".$FlightID.">
+                      <input name='numOfPass' type='hidden' value=".$numOfPass.">
+                      <input name='price' type='hidden' value=".$price.">
+                      <input name='ret_date' type='hidden' value=".$thoidiem_den.">
+                      <input name='class' type='hidden' value=".$travelClass.">
+                      <button name='book_but' type='submit' 
+                      class='btn btn-success mt-0'>
+                      </button>
                     </form>
-                   
-                    <?php
-                  }
+                    </td>    
+                    </tr>                                                   
+                    ";
                 }
               }
-
-            }catch(Exception $e) {
-              echo $e->getMessage();
             }
-          ?>
-        
-          </div>
-        </div>
-      </div>
-    </div>
-  </main>
-</body>
-</html>
-
+            ?>
+            </tbody>
+            </table>
--- backend/nhapthongtin.php
+++ backend/nhapthongtin.php
@@ -1,56 +1,38 @@
-<?php
-    if(isset($_COOKIE['FlightID'])) {
-      $FlightID = $_COOKIE['FlightID'];
-    }
-    var_dump($FlightID);
-    // echo "<br>Get<br>";
-    // var_dump($_GET);
-    // echo "<br>Post<br>";
-    // var_dump($_POST);
-    // echo "<br>";
-    
-    echo '<pre>';
-    print_r($_COOKIE);
-    echo '</pre>';
-?>
+<link rel="stylesheet" href="../asset/css/bootstrap.css">
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Flight Booking</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPC7Q0Y72iOo2b54Q4/oEBO/4+l3NPM9wU8gST1r+wSLH" crossorigin="anonymous">
-</head>
-<body>
-  <h1>Flight Booking</h1>
-  <form action="thanhtoan.php" method="post">
-    <div class="container">
-      <div class="row mb-3">
-        <div class="col">
-          <label for="name">Full Name:</label>
-          <input type="text" id="name" name="name" class="form-control" required>
+<?php
+// var_dump($_POST);
+  $listOfPassSQL = array();
+  if(isset($_POST)) {
+    $numOfPassengers = (int)$_POST['numOfPass'];
+  }  
+  ?>
+  <form action="temp.php" method="post">
+  <?php
+  for ($i = 1; $i <= $numOfPassengers; $i++) {
+    echo "Passenger $i: <br>
+    <div class='container'>
+      <div class='row mb-3'>
+        <div class='col'>
+          <label for='name'>Full Name:</label>
+          <input type='text' id='name' name='name$i' class='form-control' required>
         </div>
-        <div class="col">
-          <label for="email">Email Address:</label>
-          <input type="email" id="email" name="email" class="form-control" required>
-        </div>
-      </div>
-      <div class="row mb-3">
-        <div class="col">
-          <label for="phone">Phone Number:</label>
-          <input type="tel" id="phone" name="phone" class="form-control" required>
-        </div>
-        <div class="col">
-          <label for="passenger">Number of Passengers:</label>
-          <input type="number" id="passenger" name="passenger" min="1" class="form-control" required>
+        <div class='col'>
+          <label for='email'>Email Address:</label>
+          <input type='email' id='email' name='email$i' class='form-control' required>
         </div>
       </div>
-        <button type="submit" class="btn btn-primary">Book now</button>
-    </div>
+      <div class='row mb-3'>
+        <div class='col'>
+          <label for='phone'>Phone Number:</label>
+          <input type='tel' id='phone' name='phone$i' class='form-control' required>
+        </div>
+      </div>
+    </div>";
+  }
+?>
+  <button type="submit" class="btn btn-primary">Book now</button>
   </form>
-</body>
-</html>
 <?php
 
 if (isset($_POST['submit'])) {
@@ -59,6 +41,5 @@ if (isset($_POST['submit'])) {
     $res = mysqli_query($conn, $sql_numOfPassengers);
     $numOfPassengers = mysqli_fetch_array($res)['num'];
     // 
-    
   }   
 ?>
