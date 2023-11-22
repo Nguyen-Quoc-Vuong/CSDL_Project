0f8b04e2ca628f4dd9430b22566b1acc831de557
TaiZuon <124855937+TaiZuon@users.noreply.github.com>
2023-11-22 09:55:40 +0700

    ...

--- pages/main.php
+++ pages/main.php
@@ -15,48 +15,7 @@
 </style>
 
 <!-- <div class="main"> -->
-<div class="container-fluid" style="margin-bottom: 20px;margin-top: 20px;">
-  <div class="row"style="margin-bottom: 20px;margin-top: 20px;">
-    <div id="carouselExampleCaptions" class="carousel slide">
-      <div class="carousel-indicators">
-        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-      </div>
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img src="https://www.vietnamairlines.com/~/media/Images/HeroBannerDestination/Vietnam/Herro%20banner/PHUQUOC/Hero%20banner/2600x900/VNA-Hero_Phu%20Quoc_2x_.jpg" class="d-block w-100" alt="...">
-          <div class="carousel-caption d-none d-md-block">
-            <h5>HÈ NÀY ƯU ĐÃI 50% CHO CHUYẾN ĐI PHÚ QUỐC</h5>
-            <p>Some representative placeholder content for the first slide.</p>
-          </div>
-        </div>
-        <div class="carousel-item">
-          <img src="https://www.vietnamairlines.com/~/media/Images/HeroBannerDestination/Japan/Tokyo/Hero%20banner/2600x900/VNA-Tokio-HeroBanner-2600x900.jpg" class="d-block w-100" alt="...">
-          <div class="carousel-caption d-none d-md-block">
-            <h5>Second slide label</h5>
-            <p>Some representative placeholder content for the second slide.</p>
-          </div>
-        </div>
-        <div class="carousel-item">
-          <img src="https://www.vietnamairlines.com/~/media/Images/HeroBannerDestination/Vietnam/Herro%20banner/DALAT/Hero%20banner/2600x900/Dalat_herobanner_cao_nguyen_lam_vien_2600x900.jpg" class="d-block w-100" alt="...">
-          <div class="carousel-caption d-none d-md-block">
-            <h5>Third slide label</h5>
-            <p>Some representative placeholder content for the third slide.</p>
-          </div>
-        </div>
-      </div>
-      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-      </button>
-      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-      </button>
-    </div>
-  <!-- </div> -->
-</div>
+
 <div class="tien_ich  container">
       <!-- <div class="row"> -->
       <div class="col-xs-2"></div>
@@ -71,7 +30,7 @@
           </svg>
         </p>
         <p class="bold mt15">Dễ dàng tìm kiếm chuyến bay</p>
-        <p>Trên 100 hãng hàng không</p>
+        <p>Trên 100 điểm đến</p>
       </div>
       <div class="col-xs-2">
         <p class="box-icon">
